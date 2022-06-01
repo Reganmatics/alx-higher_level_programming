ab7f782712d4926d6c47ebee6fe4b50c76494481
Reganmatics <devreganmatics@gmail.com>
2022-06-01 09:23:08 -0700

    fix correct output

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 e_ord = ord('e')
 q_ord = ord('q')
-for i in range(ord('a'), ord('z')):
+for i in range(ord('a'), ord('z')+1):
     if i != e_ord and i != q_ord:
         print("{:c}".format(i), end="")
