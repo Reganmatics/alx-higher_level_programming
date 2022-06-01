1452d0410dd5f12d9bf5b1b93e519707113927d0
Reganmatics <devreganmatics@gmail.com>
2022-06-01 14:33:48 -0700

    quick fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -6,19 +6,3 @@ for i in range(0, 9):
             print("{}{}".format(i, j))
         else:
             print("{}{}".format(i, j), end=", ")
-
-"""
-def pal(num: int) -> int:
-    rev = int(str(num)[::-1])
-    return rev
-
-for i in range(100):
-    if i != 89:
-        if i == max(i, pal(i)) and (i == pal(i)):
-            continue
-        if i == min(i, pal(i)):
-            print("{:02d}".format(i), end=", ")
-        if i == 89:
-            print("{:02d}".format(i))
-print(pal(12487))
-"""
