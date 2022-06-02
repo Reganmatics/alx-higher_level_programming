0050a403b2c26123f2969cb825eea853dd1cb72b
Reganmatics <devreganmatics@gmail.com>
2022-06-02 16:52:18 -0700

    add task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from sys import argv
+
+
+def main():
+    if len(argv) - 1 == 0:
+        print("0 arguments.")
+    if len(argv) - 1 == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+    for i in range(len(argv) - 1):
+        print("{}: {}".format(i + 1, arg[i + 1]))
+
+
+if __name__ == "__main__":
+    main()
