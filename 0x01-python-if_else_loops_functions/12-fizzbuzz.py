b672fcdbb60459c8767632e1223b021effb59ec4
Reganmatics <devreganmatics@gmail.com>
2022-06-01 18:24:20 -0700

    add task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    if i % 3 == 0:
-        print("Fizz", end =" ")
-        continue
-    if i % 5 == 0:
-        print("Buzz", end =" ")
-        continue
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz", end = " ")
-        continue
-    if i == 100:
-        print("Buzz\n")
-        continue
-    print(i, end=" ")
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end=" ")
+            continue
+        if i % 5 == 0:
+            print("Buzz", end=" ")
+            continue
+        if i % 15 == 0:
+            print("FizzBuzz", end=" ")
+            continue
+        if i == 100:
+            print("Buzz")
+            continue
+        print(i, end=" ")
