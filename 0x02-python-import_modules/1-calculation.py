81f0c8f026e3aea3a29018a01f5e57eea28b1776
Reganmatics <devreganmatics@gmail.com>
2022-06-02 13:04:10 -0700

    fix error

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import calculator_1 as cal
+
+a = 10
+b = 5
+if __name == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
