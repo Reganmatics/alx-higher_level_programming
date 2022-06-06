8c85c0c19a0981ff07ff06ef7a4f8586c6ee68a2
Reganmatics <devreganmatics@gmail.com>
2022-06-06 07:16:49 -0700

    change code that makes print function a generator object

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 __author__ = "Reganmatics"
 
 
 def print_list_integer(my_list=[]):
-    print("{:d}".format(i) for i in my_list)
+    for i in my_list:
+        print("{:d}".format(i))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,3 @@
+# 2nd Trimester
+# second week
+# project: 0x03-python-data_structures
