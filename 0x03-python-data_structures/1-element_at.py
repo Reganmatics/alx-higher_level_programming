130b62e86ff25866716a23c7449c4ee1399a9342
Reganmatics <devreganmatics@gmail.com>
2022-06-06 16:53:02 -0700

    add task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,4 +5,6 @@ __author__ = "Reganmatics"
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    if idx >= len(my_list)
+    if idx >= len(my_list):
+        return None
+    return my_list[idx]
