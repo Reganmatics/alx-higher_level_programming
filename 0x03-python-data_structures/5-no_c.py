22183e4256b524aeb0933f251d88074949c1414d
Reganmatics <devreganmatics@gmail.com>
2022-06-06 18:28:22 -0700

    cx-cv

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for char in my_string:
-        if char != 'c' or char != 'C':
-            new_string += char
-    return new_string
+    new_string = []
+
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new_string.append(i)
+
+    return "".join(new_string)
