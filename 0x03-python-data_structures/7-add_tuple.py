eec079a7d7bd6eddd2eb1079b54746dc7c917fc7
Reganmatics <devreganmatics@gmail.com>
2022-06-06 19:33:50 -0700

    add fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 
-def add_tuple(tuple_a,=(), tuple_b=()):
+def add_tuple(tuple_a=(), tuple_b=()):
     a_1 = tuple_a[0] if len(tuple_a) >= 1 else 0
     b_1 = tuple_b[0] if len(tuple_b) >= 1 else 0
 
