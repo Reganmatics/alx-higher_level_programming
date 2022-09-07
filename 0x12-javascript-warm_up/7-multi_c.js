173f0b8891989dd0a2f91522018bdb7179e610a1
Reganmatics <devreganmatics@gmail.com>
2022-09-07 15:47:55 +0100

    make executable and fix semistandard

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,10 +4,10 @@ const process = require('process');
 
 const n = parseInt(process.argv[2]);
 
-if (isNaN(n)){
-	console.log('Missing number of occurences');
-} else if (! isNaN(n)){
-for (let i = 0; i < n; i++) {
-  console.log('C is fun');
-}
+if (isNaN(n)) {
+  console.log('Missing number of occurences');
+} else if (!isNaN(n)) {
+  for (let i = 0; i < n; i++) {
+    console.log('C is fun');
+  }
 }
