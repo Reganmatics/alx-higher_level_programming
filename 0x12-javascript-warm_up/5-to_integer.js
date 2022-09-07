70f4f4f1182a75ae272a37b2c68a53abe833ec07
Reganmatics <devreganmatics@gmail.com>
2022-09-07 10:02:27 +0100

    fix output and semistandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,4 +2,8 @@
 
 const process = require('process');
 
-console.log(isNaN(process.argv[2]));
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${parseInt(process.argv[2])}`);
+}
