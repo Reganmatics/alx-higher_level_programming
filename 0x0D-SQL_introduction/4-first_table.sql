57a272be62aa2cabdc804f4bdafee45a1c7ae0db
Reganmatics <devreganmatics@gmail.com>
2022-09-18 15:39:07 +0100

    add task 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- create table 'first_table' if not exists with description:
+-- table first_table:
+-- id INT, name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS (
+	id INT AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+);
