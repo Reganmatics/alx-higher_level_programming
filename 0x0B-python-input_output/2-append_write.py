54d935e094d583a751183ac369fd80ed590a684a
Reganmatics <devreganmatics@gmail.com>
2022-10-19 02:18:20 +0100

    fix encoding issue

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,7 +4,12 @@ task: append string to the end of a text file and return number of characters
 """
 
 
-def append_write(filename="", text=""):
+def append_write(filename="", text="", encoding="UTF8"):
+    """
+    args:
+        filename -> path to file
+        text -> string to append to end of file
+    """
     with open(filename, mode="a") as fob:
         fob.write(text)
     return len(text)
