9c9ed4a4900c9a9290f08de8116fcfe195ae5b72
Reganmatics <devreganmatics@gmail.com>
2022-12-18 20:43:18 +0100

    new

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -2,8 +2,10 @@
 -- table first_table:
 -- id INT, name VARCHAR(256)
 
-CREATE TABLE IF NOT EXISTS first_table (
-	id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(256) NOT NULL
-	PRIMARY KEY (id)
-);
+-- CREATE TABLE IF NOT EXISTS first_table (
+--	id INT NOT NULL AUTO_INCREMENT,
+--	name VARCHAR(256) NOT NULL
+--	PRIMARY KEY (id)
+-- );
+
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
